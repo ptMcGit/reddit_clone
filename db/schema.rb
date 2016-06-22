1f8a7961934313c9aa40effe06a812f66393d5ef
Zach H <zdhadgraft@gtcc.edu>
2016-06-22 18:59:27 -0400

    Added user_id to Votes table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160622201928) do
+ActiveRecord::Schema.define(version: 20160622225552) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "user_id"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20160622201928) do
     t.integer  "value"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
 end
