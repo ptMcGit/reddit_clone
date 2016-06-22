c0e75c59b254b0f531668cd390c1c0828564d6dd
Zach H <zdhadgraft@gtcc.edu>
2016-06-22 16:27:08 -0400

    Added resources for Room, Message, User, Vote

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  resources :users
+  resources :rooms
+  resources :messages
+  resources :votes
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
