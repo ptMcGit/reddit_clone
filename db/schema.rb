70b6ed42dae3893c4182b8d1390a356659a494e8
Zach H <zdhadgraft@gtcc.edu>
2016-06-23 14:11:22 -0400

    Removed `Users` in preparation for `devise`

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160623023835) do
+ActiveRecord::Schema.define(version: 20160623180737) do
 
   create_table "messages", force: :cascade do |t|
     t.integer  "user_id"
@@ -34,14 +34,6 @@ ActiveRecord::Schema.define(version: 20160623023835) do
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "votes", force: :cascade do |t|
