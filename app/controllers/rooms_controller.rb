cc26c1298b359eb40709d86294d2bd20541416f6
Zach H <zdhadgraft@gtcc.edu>
2016-06-24 15:56:37 -0400

    Added rooms#create.

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,7 +4,18 @@ class RoomsController < ApplicationController
   end
 
   def new
+    @room = Room.new(
+      )
   end
+
+  def create
+    r = Room.create(
+         approved_params
+       )
+      flash[:notice] = "Room successfully created."
+      redirect_to rooms
+  end
+
 
   def edit
   end
@@ -13,4 +24,13 @@ class RoomsController < ApplicationController
     @room = Room.find(1)
     @posts = @room.posts
   end
+
+  def approved_params
+    params.require(:room).permit(
+      :name,
+      :description
+    )
+  end
+
+
 end
