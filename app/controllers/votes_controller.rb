2c2e13f4eda4f9d079f517dcb2d358315f432e5c
Zach H <zdhadgraft@gtcc.edu>
2016-06-26 23:03:06 -0400

    Users can now only vote once.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,12 +1,19 @@
 class VotesController < ApplicationController
   def create
-    Vote.create(
-      post_id: approved_params[:post_id],
-      value:    approved_params[:value],
-      user_id: current_user.id
-    )
-    raise
+    if user_can_vote
+      Vote.create(
+        post_id: approved_params[:post_id],
+        value:    approved_params[:value],
+        user_id: current_user.id
+      )
+    else
+      flash[:notice] = "You are not allowed to vote on that post again!"
+    end
     redirect_to :back
+  end
+
+  def user_can_vote
+    ! current_user.votes.find_by(post_id: approved_params[:post_id])
   end
 
   def approved_params
