96453be3019312ff0923f8846be255281c6ad679
Zach H <zdhadgraft@gtcc.edu>
2016-06-28 13:36:59 -0400

    Added basic click event handler for up/downvote buttons.

---
+++ app/assets/javascripts/vote.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $(".upvote-btn").click(function() {
+        console.log("upvote")
+    })
+    $(".downvote-btn").click(function() {
+        console.log("downvote")
+    })
+})
