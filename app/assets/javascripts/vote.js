a04bf420e3d1558d43953c09930a731bcc2ba419
Zach H <zdhadgraft@gtcc.edu>
2016-06-28 15:12:07 -0400

    JS now pulls vote value, post value from clicks.

--- app/assets/javascripts/vote.js
+++ app/assets/javascripts/vote.js
@@ -1,13 +1,25 @@
+//      console.log($(this).parent().data("vote-value"))
+
+
 $(document).ready(function () {
-    $(".upvote-btn").click(function() {
-        console.log($(this).parent())
+    var path = "/votes"
+
+    $(".btn-upvote").click(function() {
+        current_post = $(this).parent().data("post-value")
+        vote_value = $(this).data("vote-value")
+        console.log(current_post, vote_value)
+        //$.ajax(path, method: "POST" data: { post_id: current_post, value: vote_value })
         $(this).addClass("btn-primary")
-        $(this).closest("div").find(".downvote-btn").removeClass("btn-primary")
+        $(this).closest("div").find(".btn-downvote").removeClass("btn-primary")
+
+
     })
-    $(".downvote-btn").click(function() {
-        console.log($(this))
+    $(".btn-downvote").click(function() {
+        current_post = $(this).parent().data("post-value")
+        vote_value = $(this).data("vote-value")
+                console.log(current_post, vote_value)
         $(this).addClass("btn-primary")
-        $(this).closest("div").find(".upvote-btn").removeClass("btn-primary")
+        $(this).closest("div").find(".btn-upvote").removeClass("btn-primary")
 
     })
 })
