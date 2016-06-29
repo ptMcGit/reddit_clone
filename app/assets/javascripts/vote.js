0f87d76ccb6a2ca1b16678629db96000a7e81964
Zach H <zdhadgraft@gtcc.edu>
2016-06-28 23:28:18 -0400

    Added mechanism to modify vote sum in place.

--- app/assets/javascripts/vote.js
+++ app/assets/javascripts/vote.js
@@ -11,8 +11,9 @@ var styleAsDownvote = function(target) {
 
 var updateStaticValue = function (target, vote_value) {
     var value = $(target.parent().closest("tr").find('td')[0])
-    value.text(Number(value.text()) + vote_value)
-    console.log("hey", value)
+    var index = target.parent().data("post-value")
+    value.text(staticValues[index] + vote_value)
+    console.log("hey", value, target.parent().data("post-value"))
 }
 
 var serverVoteRequest = function(vote_object, success_function) {
@@ -35,10 +36,14 @@ var serverVoteRequest = function(vote_object, success_function) {
     })
 }
 
+var staticValues = []
+
 var styleForStartingVoteValues = function() {
     var voteBoxes = $(".vote-box")
     for (i = 0; i < voteBoxes.length; i++) {
-        var vote = $(voteBoxes[i]).data('static-vote-value')
+        var vote    = $(voteBoxes[i]).data('static-vote-value')
+        var post_id = $(voteBoxes[i]).data('post-value')
+        staticValues[post_id] = vote
         console.log(vote)
         switch (vote) {
         case 1:
@@ -48,6 +53,8 @@ var styleForStartingVoteValues = function() {
         case -1:
             styleAsDownvote($(voteBoxes[i]).closest("div").find(".btn-downvote"))
             break
+        default:
+            staticValues[post_id] = 0
         }
     }
 
