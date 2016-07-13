44bbc8a43d516c03bcb46415050e370fdb051b40
Zach H <zdhadgraft@gtcc.edu>
2016-07-13 14:37:13 -0400

    Added breadcrumb-like room navbar; added link to page h1 elements.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,8 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
 
+  before_action :set_carousel
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   after_action :verify_authorized, unless: :devise_controller?
@@ -21,6 +23,10 @@ class ApplicationController < ActionController::Base
   end
 
   protected
+
+  def set_carousel
+    @carousel = Room.all.first(5)
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
