a044b371ff8a7d74b34aec283a23680c59f521a7
Zach H <zdhadgraft@gtcc.edu>
2016-07-14 00:05:51 -0400

    Added admin priveleging to user factory.

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,7 +1,17 @@
 FactoryGirl.define do
+  factory :admin do
+    user
+  end
   factory :user do
     email { Faker::Internet.safe_email }
     password "password"
+
+    factory :user_who_is_admin do
+      after(:create) do |user, evaluator|
+        create_list(:admin, 1, user: user)
+      end
+    end
   end
+
 
 end
