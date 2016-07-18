ee8b61d08e0676c3cf0aa662944c16230854a8f6
Zach H <zdhadgraft@gtcc.edu>
2016-07-18 00:12:18 -0400

    Refined moderator model, and added factory for it.

--- app/models/moderator.rb
+++ app/models/moderator.rb
@@ -1,4 +1,4 @@
 class Moderator < ActiveRecord::Base
-  has_one   :user
-  belongs_to  :room
+  belongs_to    :user
+  belongs_to    :room
 end
--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -2,6 +2,12 @@ FactoryGirl.define do
   factory :admin do
     user
   end
+
+  factory :moderator do
+    user
+    room
+  end
+
   factory :user do
     email { Faker::Internet.safe_email }
     password "password"
@@ -11,7 +17,10 @@ FactoryGirl.define do
         create_list(:admin, 1, user: user)
       end
     end
+    factory :user_who_is_mod do
+      after(:create) do |user, evaluator|
+        create_list(:moderator, 1, user: user, room: create(:room))
+      end
+    end
   end
-
-
 end
