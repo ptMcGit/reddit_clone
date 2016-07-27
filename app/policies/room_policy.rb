2864c611855da5e8c29b5f76630379930709fb97
Zach H <zdhadgraft@gtcc.edu>
2016-07-26 23:17:34 -0400

    Added policy for room show, and policies for room actions.

--- app/policies/room_policy.rb
+++ app/policies/room_policy.rb
@@ -1,32 +1,31 @@
 class RoomPolicy < ApplicationPolicy
-  attr_reader :user, :room
 
-  def initialize(user, room)
-    @user = user
-    @room = room
-  end
-
-  def index
+  def index?
     true
   end
 
-  def show
+  def show?
+    true
+  end
+
+  def new?
     true
   end
 
   def edit?
+    is_owner?
+  end
+
+  def create?
     true
+  end
+
+  def update?
+    is_owner?
   end
 
   def destroy?
     is_owner? ||
       is_admin?
   end
-
-  private
-
-  def is_owner?
-    @user == @room.user
-  end
-
 end
