307f8922deb27dee619043375fe2ae4b548b70f2
Zach H <zdhadgraft@gtcc.edu>
2016-08-31 17:06:36 -0400

    Refined user model associations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
-  has_many      :posts
-  has_many      :votes
-  has_many      :comments, through: :posts
-  has_many      :rooms
+  has_many      :posts, dependent: :nullify
+  has_many      :votes, dependent: :destroy
+  has_many      :comments, through: :posts, dependent: :destroy
+  has_many      :rooms, dependent: :nullify
 
   has_one :admin
 
