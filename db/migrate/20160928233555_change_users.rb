36d7c2d1cc997927b2ac8f6481f8e0f1984a7a9a
Zach H <zdhadgraft@gtcc.edu>
2016-09-28 19:53:05 -0400

    Modified username null constraint to relieve not null error.

---
+++ db/migrate/20160928233555_change_users.rb
@@ -0,0 +1,5 @@
+class ChangeUsers < ActiveRecord::Migration
+  def change
+    change_column_null :users, :username, true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160928160128) do
+ActiveRecord::Schema.define(version: 20160928233555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,7 +70,7 @@ ActiveRecord::Schema.define(version: 20160928160128) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "username",                            null: false
+    t.string   "username"
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
