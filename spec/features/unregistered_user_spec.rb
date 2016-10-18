5aa5dfbb8d4df9fa0e5c5e9619a98f5584cb6827
Zach H <zdhadgraft@gtcc.edu>
2016-10-18 18:49:27 -0400

    Updated unregistered user spec.

--- spec/features/unregistered_user_spec.rb
+++ spec/features/unregistered_user_spec.rb
@@ -1,19 +1,32 @@
 require 'rails_helper'
 require 'pry'
 
-describe "SignUp", type: :feature do
- it "can become a regular user" do
-    expect {
+describe "Registration", type: :feature do
+  describe "unregistered user" do
+    it "can become a regular user" do
+      @u = attributes_for(:user, password: "password")
+
       visit "/"
+
       click_on "Sign Up"
-      fill_in "user[username]", with: "testuser"
-      fill_in "user[email]", with: "test@example.com"
-      fill_in "user[password]", with: "password"
-      fill_in "user[password_confirmation]", with: "password"
-      click_on "Sign up"
-    }.
-      to change { User.count }.by 1
-    expect(current_path).
-      to eq(root_path)
+
+      expect(current_path).to eq(new_user_registration_path)
+
+      within("form#new_user") do
+        fill_in "user[username]",                 with: @u[:username]
+        fill_in "user[email]",                    with: @u[:email]
+        fill_in "user[password]",                 with: @u[:password]
+        fill_in "user[password_confirmation]",    with: @u[:password]
+        click_on "Sign up"
+      end
+
+      expect( User.find_by(email: @u[:email]) ).to be_truthy
+      expect(current_path).to eq(root_path)
+      has_link? "Log Out"
+    end
   end
+
+  describe "registered user" do
+  end
+
 end
