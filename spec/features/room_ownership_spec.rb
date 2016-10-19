bbacf8912dc9deaf7ba71ae3bee01e228cab7fcb
Zach H <zdhadgraft@gtcc.edu>
2016-10-19 00:49:35 -0400

    Added feature spec for editing rooms.

--- spec/features/room_ownership_spec.rb
+++ spec/features/room_ownership_spec.rb
@@ -25,11 +25,40 @@ describe "RoomOwners", type: :feature do
     click_on "Create Room"
     expect(current_path).to eq(new_room_path)
 
-    fill_in "room_name",            with: @r[:name]
-    fill_in "room_description",     with: @r[:description]
-    click_on "Submit"
+    expect {
+      fill_in "room_name",            with: @r[:name]
+      fill_in "room_description",     with: @r[:description]
+      click_on "Submit"
+    }.to change { Room.count }.by 1
 
     expect(current_path).to eq(room_path(Room.last.id))
+  end
+
+  it "can edit a room" do
+    @r =    room
+    @r2 =   attributes_for(:room)
+    visit "/"
+
+    within "table" do
+      click_link @r.name
+    end
+
+    expect(current_path).to eq(room_path( @r.id ))
+
+    click_on("Edit Room")
+    expect(current_path).to eq(edit_room_path( @r.id ))
+
+    fill_in "room_name",        with: @r2[:name]
+    fill_in "room_description", with: @r2[:description]
+    click_on("Submit")
+
+    @r.reload
+
+    # @r is equal to @r2
+    expect(
+      @r2.find { |k| @r[k.to_s] != @r2[k] }
+    ).to be_falsy
+
   end
 
 
