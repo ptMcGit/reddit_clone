accb7dd43786931249e7397dccadd0e604eeeaa6
Zach H <zdhadgraft@gtcc.edu>
2016-10-20 22:58:18 -0400

    Updated Post model and spec.

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,4 +11,26 @@ class Post < ActiveRecord::Base
     self.votes.sum("value")
   end
 
+  def all_comments
+    subtree = self.class.all_comments_sql(self)
+    Comment.where("id IN (#{subtree})")
+  end
+
+  def self.all_comments_sql(instance)
+    records_array = <<-SQL
+WITH RECURSIVE comment_tree(id, content, commentable_id, commentable_type, path) AS (
+  SELECT id, content, commentable_id, commentable_type, ARRAY[id]
+  FROM comments
+  WHERE commentable_type = 'Post' AND commentable_id = '#{instance.id}'
+UNION ALL
+  SELECT comments.id, comments.content, comments.commentable_id, comments.commentable_type, path || comments.id
+  FROM comment_tree
+  JOIN comments ON comments.commentable_id=comment_tree.id
+  WHERE NOT comments.id = ANY(path) AND comments.commentable_type = 'Comment'
+)
+SELECT id
+FROM comment_tree
+ORDER BY path
+  SQL
+  end
 end
--- spec/models/post_model_spec.rb
+++ spec/models/post_model_spec.rb
@@ -1,6 +1,27 @@
 require 'rails_helper'
 
 describe Post, type: :model do
+  before(:each) do
+    @p  = create(:post)
+  end
+
+  def comment_on_post(c, t=Time.now)
+    create(
+      :comment,
+      commentable_id:       c.id,
+      commentable_type:     "Post",
+      created_at:           t
+    )
+  end
+
+  def comment_on_comment(c, t=Time.now)
+    create(
+      :comment,
+      commentable_id:       c.id,
+      commentable_type:     "Comment",
+      created_at:           t
+    )
+  end
 
   it "can sum up the total votes" do
     v = create :vote, value: 1
@@ -11,4 +32,28 @@ describe Post, type: :model do
     p = Post.find(v.post_id)
     expect(p.sum).to eq 2
   end
+
+  it "#all_comments returns with nesting as expected" do
+    expect([
+             c1 = comment_on_post(          @p ),
+             c1_1 = comment_on_comment(     c1 ),
+             c1_1_1 = comment_on_comment(   c1_1 ),
+             c1_1_2 = comment_on_comment(   c1_1 ),
+             c1_2 = comment_on_comment(     c1 ),
+             c1_3 = comment_on_comment(     c1 ),
+             c1_3_1 = comment_on_comment(   c1_3),
+             c2 = comment_on_post(          @p ),
+             c3 = comment_on_post(          @p )
+           ]).to eq(
+                   @p.all_comments.to_a
+                 )
+  end
+
+  it "#all_comments returns only its own comments" do
+    c1 = comment_on_post( @p )
+    p2 = create(:post)
+    p2c1 = comment_on_post( p2 )
+    expect(@p.all_comments).to eq( [c1] )
+  end
+
 end
